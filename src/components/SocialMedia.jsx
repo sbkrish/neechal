cceeec71016ac87b2d03d67f15be4b0aa7c6a1e2
sbkrish <ranimaniyan@gmail.com>
2024-12-09 08:45:39 +0530

    link added for social media icons

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF} from 'react-icons/fa';
+import React from "react";
+import { BsInstagram, BsYoutube } from "react-icons/bs";
+import { FaFacebookF, FaLinkedinIn, FaYoutube } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
-        <div>
-            <BsTwitter />
-        </div>
-        <div>
-            <FaFacebookF />
-        </div>
-        <div>
-            <BsInstagram />
-        </div>
-    </div>
-  )
-}
+  <div>
+    <a href="https://www.instagram.com/neechaltech" target="_blank" rel="noopener noreferrer">
+      <BsInstagram />
+    </a>
+  </div>
+  <div>
+    <a href="https://www.linkedin.com/company/neechal" target="_blank" rel="noopener noreferrer">
+      <FaLinkedinIn />
+    </a>
+  </div>
+  <div>
+    <a href="https://www.facebook.com/neechaltech" target="_blank" rel="noopener noreferrer">
+      <FaFacebookF />
+    </a>
+  </div>
+  <div>
+    <a href="https://www.youtube.com/@neechal" target="_blank" rel="noopener noreferrer">
+      <BsYoutube />
+    </a>
+  </div>
+</div>
+
+  );
+};
 
 export default SocialMedia;
